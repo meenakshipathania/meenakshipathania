2f8a1fa09d467fde1676c9b495e5cb15705b875f
meenakshi <meenakship@ocodewire.com>
2022-02-12 16:42:49 +0530

    Add Departure List

--- java1.js
+++ java1.js
@@ -1,4 +1,65 @@
 
+document.querySelector('#selectCity').addEventListener("click", () => {
+  document.querySelector('.city_dropdown').classList.remove("hide");
+});
+
+document.querySelector('#outside').addEventListener("click", () => {
+  document.querySelector('.city_dropdown').classList.add("hide");
+});
+
+document.querySelector('#already').addEventListener("click", () => {
+  document.querySelector('.city_dropdown').classList.add("hide");
+});
+
+
+
+function setCity(value) {
+  document.querySelector('#country').classList.add("hide");
+  localStorage.setItem('selectedCityByUser', value.target.innerText);
+  let cityvalue = localStorage.getItem('selectedCityByUser')
+  let comma = cityvalue.indexOf(',')
+  localStorage.setItem('selectedCityByUser', value.target.innerText.substring(4,comma));
+  let depa = localStorage.getItem('selectedCityByUser')
+  document.getElementById('selectCity').innerText = depa
+
+
+  localStorage.setItem('heading1','When is your departure from') 
+  let head1 = localStorage.getItem('heading1')
+  document.getElementById('firstText').innerText = head1
+
+  localStorage.setItem('heading2','Planned') 
+  let head2 = localStorage.getItem('heading2')
+  document.getElementById('secondText').innerText = head2
+  console.log(document.querySelector('#selectCity'))
+
+  document.querySelector('.city_dropdown').classList.add("hide");
+  document.querySelector('#selectCity').value = value.target.innerText.substring(3);
+}
+let allCities = document.querySelectorAll('.city_item');
+document.querySelector('.city_input').addEventListener("input", (event) => {
+
+  if (event.target.value.length > 3) {
+      allCities.forEach((city) => {
+          let idExist = city.textContent.toLowerCase().indexOf(event.target.value.toLowerCase());
+          if (idExist < 0) {
+              city.classList.add("hide");
+          }
+          else {
+
+          }
+      })
+  }
+  else {
+      allCities.forEach((city) => {        
+          city.classList.remove("hide");
+      })
+  }
+})
+
+
+  
+
+
 window.onload = function() {
     let godesti = localStorage.getItem('destination')
     document.getElementById('desti').innerText = godesti;
